08b1fd259367bb78a011c06acf485992d431ad8c
partach <partach@gmail.com>
2026-02-08 18:54:20 +0100

    Update type_specific.py

--- custom_components/ha_felicity/type_specific.py
+++ custom_components/ha_felicity/type_specific.py
@@ -48,14 +48,14 @@ class TypeSpecificHandler:
         
     def determine_max_amperage(self, data: dict) -> float:
         if self._inverter_model in (INVERTER_MODEL_TREX_FIVE, INVERTER_MODEL_TREX_TEN):
-            phase_1 = new_data.get("ac_input_current", 0.0)
-            phase_2 = new_data.get("ac_input_current_l2", 0.0)
-            phase_3 = new_data.get("ac_input_current_l3", 0.0)
+            phase_1 = data.get("ac_input_current", 0.0)
+            phase_2 = data.get("ac_input_current_l2", 0.0)
+            phase_3 = data.get("ac_input_current_l3", 0.0)
             return max(phase_1, phase_2, phase_3)
         elif self._inverter_model == INVERTER_MODEL_TREX_FIFTY:
-            phase_1 = new_data.get("phase_a_ct_current", 0.0)
-            phase_2 = new_data.get("phase_b_ct_current", 0.0)
-            phase_3 = new_data.get("phase_c_ct_current", 0.0)
+            phase_1 = data.get("phase_a_ct_current", 0.0)
+            phase_2 = data.get("phase_b_ct_current", 0.0)
+            phase_3 = data.get("phase_c_ct_current", 0.0)
             return max(phase_1, phase_2, phase_3)
 
         _LOGGER.debug("max current not found / is None")
